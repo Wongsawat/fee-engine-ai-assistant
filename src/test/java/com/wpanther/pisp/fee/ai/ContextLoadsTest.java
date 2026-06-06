d96ccbb2280253aa5c7dff5067b075736d03a2ce
Weerachat Wongsawat <rabbit_roger@yahoo.com>
2026-06-06 10:56:27 +0700

    fix: add test JwtDecoder bean and fee-engine props for ContextLoadsTest

--- src/test/java/com/wpanther/pisp/fee/ai/ContextLoadsTest.java
+++ src/test/java/com/wpanther/pisp/fee/ai/ContextLoadsTest.java
@@ -1,10 +1,13 @@
 package com.wpanther.pisp.fee.ai;
 
 import com.wpanther.pisp.fee.ai.support.PostgresTestSupport;
+import com.wpanther.pisp.fee.ai.support.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestSecurityConfig.class)
 class ContextLoadsTest extends PostgresTestSupport {
     @Test
     void contextLoads() {
